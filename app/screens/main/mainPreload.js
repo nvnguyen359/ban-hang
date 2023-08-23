3822599a538fe99d2f00ad841b872da82fdb0db6
nvnguyen2504 <nvnguyen2504@gmail.com>
2023-08-23 15:07:04 +0700

    sendata

--- app/screens/main/mainPreload.js
+++ app/screens/main/mainPreload.js
@@ -2,6 +2,8 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 let bridge = {
     updateMessage: (callback) => ipcRenderer.on("updateMessage", callback),
+    sendData: (callback) => ipcRenderer.send("senData", callback),
+    upData: (callback) => ipcRenderer.on("upData", callback),
   };
   
   contextBridge.exposeInMainWorld("bridge", bridge);
