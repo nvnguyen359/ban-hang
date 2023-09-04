ea01717feaab0a2872b81601a709e5dd54cab4a9
nvnguyen2504 <nvnguyen2504@gmail.com>
2023-09-04 09:28:27 +0700

    cap nhat

--- app/shares/autoUpdater.js
+++ app/shares/autoUpdater.js
@@ -2,9 +2,52 @@ const { autoUpdater } = require("electron-updater");
 const { getPrinters } = require("./lib");
 autoUpdater.autoDownload = true;
 autoUpdater.autoInstallOnAppQuit = true;
+function newUpdate(app) {
+  let mes = "";
+  /*New Update Available*/
+  return new Promise((res, rej) => {
+    autoUpdater.on("update-available", (info) => {
+      mes = `Cập nhật có sẵn. Phiên bản hiện tại ${app.getVersion()}`;
+      let pth = autoUpdater.downloadUpdate();
+      mes = pth;
+      res(mes);
+    });
+  });
+}
+function checkupdateNotAvailable(curWindow, app) {
+  return new Promise((res, rej) => {
+    let mes = "";
+    autoUpdater.on("update-not-available", (info) => {
+      mes = `Cập nhật không có sẵn. Phiên bản hiện tại ${app.getVersion()}`;
+      curWindow.showMessage(mes);
+      res(mes);
+    });
+  });
+}
+function updateDownloaded(app) {
+  let mes = "";
+  /*New Update Available*/
+  return new Promise((res, rej) => {
+    autoUpdater.on("update-available", (info) => {
+      mes = `Cập nhật có sẵn. Phiên bản hiện tại ${app.getVersion()}`;
+      let pth = autoUpdater.downloadUpdate();
+      mes = pth;
+    });
+    res(mes);
+  });
+}
+function downloadComplete(app) {
+  return new Promise((res, rej) => {
+    let mes = "";
+    autoUpdater.on("update-downloaded", (info) => {
+      mes = `Đã tải xuống bản cập nhật. Phiên bản hiện tại ${app.getVersion()}`;
+      res(mes);
+    });
+  });
+}
 /** event new update,... */
 function eventsAutoUpdate(curWindow, app) {
-  const mes = "";
+  let mes = "";
   /*New Update Available*/
   autoUpdater.on("update-available", (info) => {
     mes = `Cập nhật có sẵn. Phiên bản hiện tại ${app.getVersion()}`;
@@ -39,6 +82,13 @@ function checkForUpdates(curWindow, app) {
   getPrinters().then((data) => {
     curWindow.upData(JSON.stringify(data));
   });
-  return {mes,ver:app.getVersion()};
+  return { mes, ver: app.getVersion() };
 }
-module.exports = { checkForUpdates, eventsAutoUpdate };
+module.exports = {
+  checkForUpdates,
+  eventsAutoUpdate,
+  newUpdate,
+  checkupdateNotAvailable,
+  updateDownloaded,
+  downloadComplete
+};
